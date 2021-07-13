fb58e3c2ae0c7576f0e2b8d6e1132591f5f944b5
Ashton Monzi <anmonzi@gmail.com>
2021-07-13 15:32:33 -0500

    Added Classes for each resource stored in a models folder

---
+++ models/location.py
@@ -0,0 +1,10 @@
+class Location():
+    """Location Class
+    """
+    # Class initializer. It has 5 custom parameters, with the
+    # special `self` parameter that every METHOD on a CLASS
+    # needs as the first parameter.
+    def __init__(self, id, name, address):
+        self.id = id
+        self.name = name
+        self.address = address
