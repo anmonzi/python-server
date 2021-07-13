7bdef1b54504bfeca9bceb4730e582e579c360b0
Ashton Monzi <anmonzi@gmail.com>
2021-07-13 10:25:37 -0500

    Initial DELETE operation works on animals. Adding DELETE method to other resource types

--- animals/request.py
+++ animals/request.py
@@ -81,3 +81,21 @@ def create_animal(animal):
 
     # Return the dictionary with `id` property added
     return animal
+
+
+def delete_animal(id):
+    """Delete an animal by Id
+    """
+    # Initial -1 value for animal index, in case one isn't found
+    animal_index = -1
+
+    # Iterate the ANIMALS list, but use enumerate() so that you
+    # can access the index value of each item
+    for index, animal in enumerate(ANIMALS):
+        if animal["id"] == id:
+            # Found the anima. Store the current index.
+            animal_index = index
+
+    # If the animal was found, use pop(int) to remove it from the list
+    if animal_index >= 0:
+        ANIMALS.pop(animal_index)
